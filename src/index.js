0e6ac015f97d5cdae4f5d1458df032a228228f18
Vojtěch Keder <vojta.keder@gmail.com>
2024-12-21 20:47:53 +0100

    Implemented Quiz class and Quiz question class to handle the logic.

--- src/index.js
+++ src/index.js
@@ -147,6 +147,202 @@ class RestCountriesDataPreprocessor {
     }
 }
 
+/** The quiz class which manages all the quiz data **/
+class Quiz {
+    constructor(data) {
+        this.data = data;
+        this.score = 0;
+        this.currentQuestionIdx = 0;
+        this.questions = [];
+        this.init();
+    }
+
+    /** Init function **/
+    init() {
+        const indexes = Array.from({ length: this.data.length },
+            (_, i) => i);
+        const randomCorrectAnswersIdxs = this.getRandomCorrectAnswersIdxs(indexes);
+
+        // Remove the correct answers from the array of indexes, so the wrong options can't contain them
+        for(let i = 0; i < randomCorrectAnswersIdxs.length; i++) {
+            let idxToRemove = indexes.indexOf(randomCorrectAnswersIdxs[i]);
+            indexes.splice(idxToRemove, 1);
+        }
+
+        const randomWrongAnswers = this.getRandomWrongAnswersIdxs(indexes);
+
+        for(let i = 0; i < constants.QUIZ_QUESTION_COUNT; i++) {
+            this.questions.push(
+                new QuizQuestion(randomCorrectAnswersIdxs[i], randomWrongAnswers[i], data)
+            );
+        }
+    }
+
+    /** Return the overall score **/
+    getOverallScore() {
+        return this.score;
+    }
+
+    /** Update current question index **/
+    updateCurrentQuestionIndex() {
+        this.currentQuestionIdx += 1;
+    }
+
+    /** Returns starting hints for current question **/
+    getCurrentQuestionStartHints() {
+        return this.questions[this.currentQuestionIdx].getStartHints();
+    }
+
+    /** Returns additional hint for current question **/
+    getCurrentQuestionNextHint() {
+        return this.questions[this.currentQuestionIdx].getHint();
+    }
+
+    /** Returns indexes of all possible answers for question **/
+    getCurrentQuestionAnswersIdxs() {
+        return this.questions[this.currentQuestionIdx].getShuffeledAnswerIdxs();
+    }
+
+    /** Check the answer and update the questions parameters and the overall score **/
+    checkAnswer(answerIdx) {
+        let currentQuestion = this.questions[this.currentQuestionIdx];
+
+        if(answerIdx === currentQuestion.correctAnswerIdx) {
+            currentQuestion.setCorrect();
+            currentQuestion.computePoints();
+            this.score += currentQuestion.getPoints();
+        }
+    }
+
+    /** Check if the answers was correct - use only after Check answer! **/
+    isAnswerCorrect(answerIdx) {
+        return this.questions[this.currentQuestionIdx].getWasCorrect();
+    }
+
+    /** Return whether it is the last question of the quiz **/
+    isLastQuestion() {
+        return this.currentQuestionIdx + 1 === constants.QUIZ_QUESTION_COUNT;
+    }
+
+    /** Returns an array of distinct ID's, which represent correct answers **/
+    getRandomCorrectAnswersIdxs(indexes) {
+        // Shuffle the array of indexes
+        let randomCountryIDXS = shuffleArray(indexes);
+
+        // Return only the first n ID's, up to the question count in quiz
+        return randomCountryIDXS.slice(0, constants.QUIZ_QUESTION_COUNT);
+    }
+
+    /** Returns an array of arrays of ID's, which represent wrong options for each correct **/
+    getRandomWrongAnswersIdxs(indexes) {
+        const randomWrongAnswers = [];
+
+        for(let i = 0; i < constants.QUIZ_QUESTION_COUNT; i++) {
+            const wrongAnswers = shuffleArray(indexes);
+            randomWrongAnswers.push(wrongAnswers.slice(0, constants.QUIZ_QUESTION_OPTION_COUNT - 1));
+        }
+
+        return randomWrongAnswers;
+    }
+}
+
+/** Class representing information about a certain question, such as the correct answer and more **/
+class QuizQuestion {
+    constructor(correctAnswerIdx, wrongAnswersIdxs, data) {
+        this.correctAnswerIdx = correctAnswerIdx;
+        this.wrongAnswersIdxs = wrongAnswersIdxs;
+        this.startHints = null;
+        this.additionalHints = null;
+        this.hintsUsed = 0;
+        this.nextHintIdx = 0;
+        this.allAnswersShuffeledIdxs = null;
+        this.wasCorrect = false;
+        this.points = 0;
+        this.init(data);
+    }
+
+    init(data) {
+        this.setHints(data);
+        const toShuffle = this.wrongAnswersIdxs;
+        toShuffle.push(this.correctAnswerIdx);
+        this.allAnswersShuffeledIdxs = shuffleArray(toShuffle);
+    }
+
+    setHints(data) {
+        const correctAnswerData = data[this.correctAnswerIdx];
+
+        this.startHints = {
+            "Region": correctAnswerData.region,
+            "Population": correctAnswerData.population,
+            "Landlocked": correctAnswerData.landlocked,
+            "Area": correctAnswerData.area,
+            "Gini index": correctAnswerData.gini,
+            "UN member:": correctAnswerData.unMember,
+            "Driving side": correctAnswerData.drivingSide,
+            "Phone prefix": correctAnswerData.phonePrefix,
+            "Currency symbol": correctAnswerData.currencySymbol,
+        };
+
+        this.additionalHints = {
+            "Subregion": correctAnswerData.subregion,
+            "Car license plate signs": correctAnswerData.carPlate,
+            "Borders": correctAnswerData.borders,
+            "Capital(s)": correctAnswerData.capital,
+            "FlagURL": correctAnswerData.flag
+        };
+    }
+
+    /** Sets question answer as correct **/
+    setCorrect() {
+        this.wasCorrect = true;
+    }
+
+    /** Compute points based on answer and hints **/
+    computePoints() {
+        if(this.wasCorrect) {
+            this.points += constants.QUIZ_QUESTION_MAX_POINTS -
+                this.hintsUsed * constants.QUIZ_QUESTION_HINT_PENALIZATION;
+        }
+    }
+
+    /** Return shuffeled possible answers indexes **/
+    getShuffeledAnswerIdxs() {
+        return this.allAnswersShuffeledIdxs;
+    }
+
+    /** Return points obtained in the question **/
+    getPoints() {
+        return this.points;
+    }
+
+    /** Return if the question was correct **/
+    getWasCorrect() {
+        return this.wasCorrect;
+    }
+
+    /** Returns start hints **/
+    getStartHints() {
+        return this.startHints;
+    }
+
+    /** Returns the next hint **/
+    getHint() {
+        if(this.hintsUsed >= constants.QUIZ_QUESTION_MAX_HINTS) {
+            console.log("No more hints...");
+            return null;
+        }
+
+        const keys = Object.keys(this.additionalHints);
+        const nextHintVal = this.additionalHints[keys[this.nextHintIdx]];
+        const returnVal = [keys[this.nextHintIdx], nextHintVal]
+
+        this.hintsUsed += 1;
+        this.nextHintIdx += 1;
+
+        return returnVal;
+    }
+}
+
 $(document).ready(function () {
     let dataPreprocessor = new RestCountriesDataPreprocessor();
 
